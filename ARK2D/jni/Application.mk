638e9545537ec358cca7715b6fb85fbc73f87d51
Ashley Gwinnell <info@ashleygwinnell.co.uk>
2012-02-17 23:05:46 +0000

    deleting jni files

--- ARK2D/jni/Application.mk
+++
@@ -1,5 +0,0 @@
-APP_PROJECT_PATH := /Users/ashleygwinnell/Dropbox/ark2d/ARK2D
-APP_BUILD_SCRIPT := /Users/ashleygwinnell/Dropbox/ark2d/ARK2D/build/android/Android.mk
-NDK_APP_OUT=/Users/ashleygwinnell/Dropbox/ark2d/ARK2D/build/android
-NDK_PROJECT_PATH=/Users/ashleygwinnell/Dropbox/ark2d/ARK2D
-APP_STL := stlport_static
