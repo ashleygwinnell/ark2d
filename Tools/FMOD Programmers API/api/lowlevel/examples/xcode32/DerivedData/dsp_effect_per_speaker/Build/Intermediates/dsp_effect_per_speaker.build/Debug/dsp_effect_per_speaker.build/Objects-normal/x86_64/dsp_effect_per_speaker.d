82dc0246ae374f6512a1d7a599eaa5840dcc11b0
Ashley Gwinnell <ashley@forceofhab.it>
2016-08-09 10:23:34 +0100

    Remove FMOD api examples.

--- Tools/FMOD Programmers API/api/lowlevel/examples/xcode32/DerivedData/dsp_effect_per_speaker/Build/Intermediates/dsp_effect_per_speaker.build/Debug/dsp_effect_per_speaker.build/Objects-normal/x86_64/dsp_effect_per_speaker.d
+++
@@ -1,7 +0,0 @@
-dependencies: \
-  /Users/ashleygwinnell/Dropbox/ark2d/Tools/FMOD\ Programmers\ API/api/lowlevel/examples/dsp_effect_per_speaker.cpp \
-  ../../../lowlevel/inc/fmod.hpp ../../../lowlevel/inc/fmod_common.h \
-  ../../../lowlevel/inc/fmod_codec.h ../../../lowlevel/inc/fmod_dsp.h \
-  ../../../lowlevel/inc/fmod.h \
-  /Users/ashleygwinnell/Dropbox/ark2d/Tools/FMOD\ Programmers\ API/api/lowlevel/examples/common.h \
-  /Users/ashleygwinnell/Dropbox/ark2d/Tools/FMOD\ Programmers\ API/api/lowlevel/examples/common_platform.h
